746464e0226209deeb8f4d433971512f1f87714f
DatNguyen <thathaivuong@gmail.com>
2025-05-07 08:54:22 +0700

    update-route-user

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,6 @@ Route::post('signup',[PagesController::class, 'postSignup']);
 Route::get('logout',[PagesController::class, 'getLogout']);
 
 Route::get('/', [PagesController::class, 'trangchu']);
-
 Route::get('/user/{id}',[PagesController::class, 'userDetails']);
 Route::get('/trangcanhan',[PagesController::class, 'userDetails']);
 Route::middleware('auth')->group(function() {
@@ -34,3 +33,29 @@ Route::middleware('auth')->group(function() {
         Route::post('/changepassword', [PagesController::class, 'postChangePassword']);
     });
 });
+
+
+Route::middleware('staff')->group(function() {
+    Route::prefix('admin')->group(function(){
+
+       
+
+        
+       
+        Route::prefix('user')->group(function() {
+            Route::get('/list', [UserController::class, 'list']);
+            Route::get('/create', [UserController::class, 'getCreate']);
+            Route::post('/create', [UserController::class, 'postCreate']);
+            Route::get('/edit/{id}', [UserController::class, 'getEdit']);
+            Route::post('/edit/{id}', [UserController::class, 'postEdit']);
+            Route::get('/active/{id}', [UserController::class, 'postActive']);
+            Route::get('/block/{id}', [UserController::class, 'postNoActive']);
+            Route::get('/delete/{id}',[UserController::class, 'getDelete']);
+        });
+       
+       
+       
+    });
+});
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
